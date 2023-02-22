c0e7b8520752d8b5786b0632d4c6ae316b867e7a
dracolars <dracolars@users.noreply.github.com>
2023-02-22 13:19:43 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
+
 const contactEmail = nodemailer.createTransport({
     host: 'smtp.titan.email',
     port: 465,
